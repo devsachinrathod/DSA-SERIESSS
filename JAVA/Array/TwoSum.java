979cd00bf2b940f1fb3be7b51f6c9137540b4102
Sachin Rathod <sr5631080@gmail.com>
2026-08-27 23:11:05 +0530

    Contructersig-dsa

---
+++ JAVA/Array/TwoSum.java
@@ -0,0 +1,39 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+//9 -2 = 7
+//9 -7 = 2
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                result[0] = map.get(complement);
+                result[1] = i;
+                break;
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        TwoSum obj = new TwoSum();
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 15;
+
+        int[] result = obj.twoSum(nums, target);
+
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
