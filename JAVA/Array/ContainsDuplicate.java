3320828ac0c974a8247dde755febaae41c024199
Sachin Rathod <sr5631080@gmail.com>
2026-08-28 22:46:49 +0530

    Duplicate values checks

---
+++ JAVA/Array/ContainsDuplicate.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
+
+class ContainsDuplicate{
+  
+
+  public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+
+  }
+  public static void main(String[] args) {
+    ContainsDuplicate cd = new ContainsDuplicate();
+    int[] nums = {1,2,3,4,5,6,7,8,9,10};
+    System.out.println(cd.containsDuplicate(nums));
+  }
+}
