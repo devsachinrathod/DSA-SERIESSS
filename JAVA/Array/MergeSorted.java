83059aadf7cdd9afe05bcee8e7bee6bd1a5211c7
Sachin Rathod <sr5631080@gmail.com>
2026-08-29 15:42:32 +0530

    addeda sorted merge array'

---
+++ JAVA/Array/MergeSorted.java
@@ -0,0 +1,33 @@
+class MergeSorted {
+
+    public void MergeArr(int[] arr, int[] arr2) {
+
+        int[] mergeArray = new int[arr.length + arr2.length];
+
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            mergeArray[index] = arr[i];
+            index++;
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            mergeArray[index] = arr2[i];
+            index++;
+        }
+
+        for (int num : mergeArray) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        MergeSorted obj = new MergeSorted();
+
+        int[] arr = {1, 3, 5};
+        int[] arr2 = {2, 4, 6};
+
+        obj.MergeArr(arr, arr2);
+    }
+}
