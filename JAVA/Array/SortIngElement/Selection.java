7ec537bebf24747c88bc38683f3ef902c65bed49
Sachin Rathod <sr5631080@gmail.com>
2026-08-31 21:59:30 +0530

    added isMonotonic

---
+++ JAVA/Array/SortIngElement/Selection.java
@@ -0,0 +1,26 @@
+class Selection {
+
+
+    public static void main(String[]  args){
+         int[] arr = {4,5,3,2,1};
+         for(int i = 0; i < arr.length -1 ; i++){
+            int min = i;//0
+
+        for(int j = i; j < arr.length; j++){
+                if(arr[j] < arr[min]){
+                  min = j;
+                }
+            }
+            int temp = arr[i];4
+            arr[i] = arr[min];
+            arr[min] = temp;
+          
+         }
+         for(int num : arr){
+            System.out.print(num + " ");
+         }
+            
+
+
+    }
+}
