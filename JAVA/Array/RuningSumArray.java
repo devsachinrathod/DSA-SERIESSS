b58f88da794ecda162f8623712f0d399f7fb1ec0
Sachin Rathod <sr5631080@gmail.com>
2026-09-16 23:59:17 +0530

    ad

--- JAVA/Array/RuningSumArray.java
+++ JAVA/Array/RuningSumArray.java
@@ -9,6 +9,27 @@ public class RuningSumArray {
         }
         return arr;
     }
+
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+
+            if (map.containsKey(num)) {
+                return true;
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        return false;
+    }
+}
+
+    sdds
 }
 public class RuningSumArray {
     public int[] runningSum(int[] nums) {
