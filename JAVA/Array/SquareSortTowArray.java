63540195deab7d37ac7fc07ba7f983c57738beab
Sachin Rathod <sr5631080@gmail.com>
2026-09-18 15:37:40 +0530

    aded

--- JAVA/Array/SquareSortTowArray.java
+++ JAVA/Array/SquareSortTowArray.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+ import java.util.Arrays;
 
 class Solution {
     // Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
@@ -42,4 +42,6 @@ class Solution {
         s.merge(nums1, m, nums2, n);
         // Output: [1, 2, 2, 3, 5, 6]
     }
-}
+} 
+    
+
